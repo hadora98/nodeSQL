683c00ef9098333ef172cfdd0923622a14764fa0
hadora98 <65473166+hadora98@users.noreply.github.com>
2021-11-18 08:27:34 +0300

    Create index2.js

---
+++ index2.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+   
+    var sql = require("mssql");
+
+    // config for your database
+    var config = {
+        user: 'rdsuser',
+        password: 'Ha@74740170',
+        server: '182.160.11.82', 
+        database: 'database1' 
+    };
+
+    // connect to your database
+    sql.connect(config, function (err) {
+    
+        if (err) console.log(err);
+
+        // create Request object
+        var request = new sql.Request();
+           
+        // query to the database and get the records
+        request.query('select * from mytable2', function (err, recordset) {
+            
+            if (err) console.log(err)
+
+            // send records as a response
+            res.send(recordset);
+            
+        });
+    });
+});
+
+var server = app.listen(5000, function () {
+    console.log('Server is running..');
+});
